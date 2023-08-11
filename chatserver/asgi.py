8c6b0acb469790a879652b34f4078570896369ea
ernesthenry <henry38ernest@gmail.com>
2023-08-11 16:15:06 +0300

    Add routing configuration

--- chatserver/asgi.py
+++ chatserver/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for chatserver project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatserver.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+    }
+)
